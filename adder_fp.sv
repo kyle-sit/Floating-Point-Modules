677e5756cb6743a5aa910412d1a7335bcc496082
Kyle Sit <kyle.sit.sd@gmail.com>
2017-11-07 16:51:23 -0800

    Initial files

---
+++ adder_fp.sv
@@ -0,0 +1,12 @@
+/* File containing module code for a floating point adder
+*/
+
+//adder module
+module adder_fp(
+	input logic clk, start, op,
+	input logic [31:0] A, B,
+	output logic ready, busy,
+	output logic [31:0] Y
+);
+
+endmodule
