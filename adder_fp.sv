455371705e4a0c9af45130a5e7537f3e40ad6acd
Kyle Sit <kyle.sit.sd@gmail.com>
2017-11-12 23:19:00 -0800

    Add files via upload
    need to determine sign of output, consider redesigning

--- adder_fp.sv
+++ adder_fp.sv
@@ -1,3 +1,4 @@
+
 /* File containing module code for a floating point adder
 */
 
@@ -6,14 +7,13 @@ module adder_fp(
 	input logic clk, start, op,
 	input logic [31:0] A, B,
 	output logic ready, busy,
-	output logic [31:0] Y
-	
+	output logic [31:0] Y	
 );
 
 	logic [1:0] state; // 3 states = 2 bits
 	parameter idle = 2'd0, active = 2'd1, finished = 2'd2;
 	logic A_sign, B_sign;
-	logic [22:0] A_mantissa, B_mantissa;
+	logic [22:0] A_mantissa, B_mantissa, final_mantissa;
 	logic [7:0] A_exponent, B_exponent;
 	logic [24:0] output_mantissa;
 	
@@ -25,14 +25,13 @@ module adder_fp(
 				end
 				else begin
 					ready <= 0;
-					busy <=0;
+					busy <= 0;
+					Y <= 0;
 					state <= state;
 				end
 			end
 			active: begin
-				busy <= 1;
-				//A <= A;
-				//B <= B;
+				busy = 1;
 				A_sign <= A[31];
 				A_exponent <= A[30:23];
 				A_mantissa <= A[22:0];
@@ -42,42 +41,73 @@ module adder_fp(
 
 				// infinity = 8'b11111111 for exponent, 0 for mantissa
 				// NaN = 8'b11111111 for exponent, xxxx... for mantissa
-				if((A_exponent == 8'b11111111 and A_mantissa > 0) or (B_exponent == 8'b11111111 and B_mantissa > 0)) begin
-					Y <= {0,8'b11111111,22'b0,1};
+				//case if either input is NaN
+				if(((A_exponent == 8'b11111111) && (A_mantissa > 0)) || ((B_exponent == 8'b11111111) && (B_mantissa > 0))) begin
+					Y <= {1'b0,8'b11111111,22'b0,1'b1};
 				end
-				else if ((A_exponent == 8'b11111111 and A_mantissa == 0) and (B_exponent == 8'b11111111 and B_mantissa == 0)) begin
+				//case if both inputs are infinity
+				else if ((A_exponent == 8'b11111111 && A_mantissa == 0) && (B_exponent == 8'b11111111 && B_mantissa == 0)) begin
 					if (A_sign == B_sign) begin
 						Y <= A;
 					end
 					else begin
-						Y <= {0,8'b11111111,22'b0,1};
+						Y <= {1'b0,8'b11111111,22'b0,1'b1};
 					end
 				end
-				else if (A_exponent == 8'b11111111 and A_mantissa == 0) begin
+				//case if A input is infinity
+				else if (A_exponent == 8'b11111111 && A_mantissa == 0) begin
 					Y <= A;
 				end
-				else if (B_exponent == 8'b11111111 and B_mantissa == 0) begin
+				//case if B input is infinity
+				else if (B_exponent == 8'b11111111 && B_mantissa == 0) begin
 					Y <= B;
 				end
-				
 				// now we use the adding algorithm
-				
 				else if (A_exponent < B_exponent) begin
-					A_mantissa >> (B_exponent - A_exponent);
+					A_mantissa <= (A_mantissa >> (B_exponent - A_exponent));
+					//subtraction
+					if (op) begin
+					end
+					//addition
+					else begin
+						output_mantissa = A_mantissa + B_mantissa;
+						//overflow into msb
+						if (output_mantissa[24] != 0) begin
+							final_mantissa <= output_mantissa[24:2];
+							if (output_mantissa[1] == 1) begin
+								final_mantissa <= final_mantissa + 1;
+							end
+							Y <= 
+						end
+						//overflow by 1 bit
+						else if (output_mantissa[23] != 0) begin
+							final_mantissa <= output_mantissa[23:1];
+							if (output_mantissa[0] == 1) begin
+								final_mantissa <= final_mantissa + 1;
+							end
+							//sign?
+						end
+						//no overflow
+						else begin
+						end
+					end
 				end
-				else if (B_exponent < A_exponent) begin
-				end
-				
-				else begin
+				else begin //(B_exponent <= A_exponent)
 				end
 				
 				state = finished;
 			end
 			finished: begin
 				ready <= 1;
-				Y <= 0;
+				//Y <= 0;
 				state <= idle; 
 			end
+			default: begin
+				ready <= 0;
+				busy <= 0;
+				Y <= 0;			
+			end
+		endcase
 	end
 
 endmodule
